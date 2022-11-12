0f881ab113712c2f6b0722b493b716a335134a96
slipneff <slipneff@mail.ru>
2022-11-12 16:51:23 +0300

    x

--- LinAlgLib/array/TensorS.py
+++ LinAlgLib/array/TensorS.py
@@ -26,7 +26,8 @@ class TensorList:
                     break 
         else:
             pass
-    
+
+
     def __repr__(self) -> str:
         return f"FastLinAlg.TensorList:{self.array}"
         
@@ -45,6 +46,15 @@ class Tensor(TensorList):
     def __init__(self, _list: list[list]) -> None:
         self.array = _list
         Tensor.shape(self.array)
+    
+
+    @classmethod
+    def T(cls,array):
+        transpose_matrix = [[0 for j in range(cls._paramR)] for i in range(cls._paramC)]
+        for i in range(cls._paramR):
+            for j in range(cls._paramC):
+                transpose_matrix[i][j] = array[i][j]
+        return transpose_matrix
 
 
 
