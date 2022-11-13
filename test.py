d986206215b4d7bdc8c5bddc9d5e8afb8353d83b
slipneff <slipneff@mail.ru>
2022-11-13 19:20:09 +0300

    x

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from LinAlgLib.array.TensorS import *
 import time 
+import numpy as np
 a = [float(i) for i in range(10000000)]
 b = TensorList(a)
 
@@ -24,3 +25,8 @@ print("--- %s seconds --- Python" % (time.time() - start_time))
 start_time = time.time()
 g = [a[i] * a[i] for i in range(len(a))]
 print("--- %s seconds --- Python Generator" % (time.time() - start_time))
+
+
+start_time = time.time()
+n = np.array(a) * np.array(a)
+print("--- %s seconds --- Numpy" % (time.time() - start_time))
