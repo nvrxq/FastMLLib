0c231b42afb2b34ac6c3ea1886384db2b69845fb
slipneff <slipneff@mail.ru>
2022-12-06 14:27:39 -0500

    x

--- LinAlgLib/array/TensorS.py
+++ LinAlgLib/array/TensorS.py
@@ -44,8 +44,23 @@ class TensorList:
         else:
             return self.array[column][rows]
 
-    def Dot(self, object2):
-        return LinAlgLib_C.Dot(self.array, object2.array)
+    def Dot(self, object1):
+        # return LinAlgLib_C.Dot(self.array, object2.array)
+        if self._paramC != object1._paramC:
+            return 0
+
+        return sum(i[0] * i[1] for i in zip(self.array, object1.array))
+
+    def median(self):
+        med = 0
+        if self._paramR % 2 == 0:
+            for i in self.array:
+                med += i
+            return med/self._paramR
+        else:
+            return list(sorted(self.array))[int(self._paramR/2)]
+
+
 
 
 
@@ -63,9 +78,34 @@ class Tensor(TensorList):
         result = [[self.array[j][i] for j in range(self._paramR)] for i in range(self._paramC)]
         return Tensor(result)
     
-    #def d(self):
-     #   return LinAlgLib_C.d(self.array)
 
+    def Dot(self, object1):
+        if self._paramC != object1._paramR:
+            print ("Cannot multiply the two matrices. Incorrect dimensions.")
+            return 
+
+        C = [[0 for row in range(object1._paramC)] for col in range(self._paramR)]
+
+        for i in range(self._paramR):
+            for j in range(object1._paramC):
+                for k in range(self._paramC):
+                    C[i][j] += self.array[i][k] * object1.array[k][j]
+        return C
+
+    def flatten(self):
+        return [i for j in self.array for i in j]
+
+    def median(self):
+        flatten_object = self.flatten()
+        med = 0
+        if len(flatten_object) % 2 == 0:
+            for i in flatten_object:
+                med += i
+            return med / len(flatten_object)
+        else:
+            return sorted(flatten_object)[int(len(flatten_object)/2)]
+
+            # median for axises and reshape
 
 
 
--- test.py
+++ test.py
@@ -1,14 +1,6 @@
 from LinAlgLib.array.TensorS import *
 import time 
 import numpy as np
-a = [float(i) for i in range(10000000)]
-b = TensorList(a)
-
-
-start_time = time.time()
-c = b.Dot(b)
-print("--- %s seconds --- LinAlgLib" % (time.time() - start_time))
-
 
 def dotd(a,b):
     sol = []
@@ -16,17 +8,42 @@ def dotd(a,b):
         sol.append(a[i] * b[i])
     return sol
 
-
-start_time = time.time()
-p = dotd(a,a)
-print("--- %s seconds --- Python" % (time.time() - start_time))
-
-
-start_time = time.time()
-g = [a[i] * a[i] for i in range(len(a))]
-print("--- %s seconds --- Python Generator" % (time.time() - start_time))
+def test():
+    a = [float(i) for i in range(10000000)]
+    b = TensorList(a)
 
 
-start_time = time.time()
-n = np.array(a) * np.array(a)
-print("--- %s seconds --- Numpy" % (time.time() - start_time))
+    start_time = time.time()
+    c = b.Dot(b)
+    print("--- %s seconds --- LinAlgLib" % (time.time() - start_time))
+
+
+
+
+
+    start_time = time.time()
+    p = dotd(a,a)
+    print("--- %s seconds --- Python" % (time.time() - start_time))
+
+
+    start_time = time.time()
+    g = [a[i] * a[i] for i in range(len(a))]
+    print("--- %s seconds --- Python Generator" % (time.time() - start_time))
+
+
+    start_time = time.time()
+    n = np.array(a) * np.array(a)
+    print("--- %s seconds --- Numpy" % (time.time() - start_time))
+
+a = Tensor([[1,2],[3,4],[5,6]])
+print(a)
+print(a.shape())
+
+b = [[1,2],[1,1,10]]
+li = list(i for j in b for i in j)
+c = Tensor(b)
+print(b,li,c.flatten(),c.median())
+
+p = TensorList([1,2,2])
+
+print(p.median())
